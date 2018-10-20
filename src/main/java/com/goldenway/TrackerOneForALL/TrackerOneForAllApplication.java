70a658c82955e42642e762ca8656854b4a18095f
lvoix <bargachyoussef09@gmail.com>
2018-10-20 17:39:35 +0100

    first commit

---
+++ src/main/java/com/goldenway/TrackerOneForALL/TrackerOneForAllApplication.java
@@ -0,0 +1,12 @@
+package com.goldenway.TrackerOneForALL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerOneForAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerOneForAllApplication.class, args);
+	}
+}
---
+++ src/test/java/com/goldenway/TrackerOneForALL/TrackerOneForAllApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goldenway.TrackerOneForALL;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrackerOneForAllApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
